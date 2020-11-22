1868aeb7c5db6c7c652c410afca1b423ce985718
penglin <penglin@himango.cn>
2020-11-22 21:16:36 +0800

    Initial commit

---
+++ sample/src/main/java/me/lake/librestreaming/sample/ui/AlwaysMarqueeTextView.java
@@ -0,0 +1,26 @@
+package me.lake.librestreaming.sample.ui;
+
+import android.content.Context;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class AlwaysMarqueeTextView extends AppCompatTextView {
+  
+    // com.duopin.app.AlwaysMaguequeScrollView  
+    public AlwaysMarqueeTextView(Context context) {
+  
+        super(context);  
+  
+        // TODO Auto-generated constructor stub  
+    }  
+  
+
+    @Override  
+    public boolean isFocused() {  
+  
+        return true;  
+  
+    }  
+  
+
+}  
